aabc47df237b6f35ac198924fa7a45c52f625445
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-12-10 22:16:37 -0300

    Usuarios Microservice

---
+++ src/main/java/br/com/acrtech/planningpoker/usuarioms/UsuarioMsApplication.java
@@ -0,0 +1,12 @@
+package br.com.acrtech.planningpoker.usuarioms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class UsuarioMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/acrtech/planningpoker/usuarioms/UsuarioMsApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.acrtech.planningpoker.usuarioms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class UsuarioMsApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
