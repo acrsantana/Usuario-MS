2512c4ae01fe39230fdd4cc4f0cd896c306730d4
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-12-19 16:20:58 -0300

    Documentação da API de Usuários

--- src/main/java/br/com/acrtech/planningpoker/usuarioms/UsuarioMsApplication.java
+++ src/main/java/br/com/acrtech/planningpoker/usuarioms/UsuarioMsApplication.java
@@ -1,8 +1,12 @@
 package br.com.acrtech.planningpoker.usuarioms;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +14,25 @@ public class UsuarioMsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsuarioMsApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+
+            .info(new Info()
+
+                .title("Planning Poker/Usuario MS")
+
+                .version("1.0")
+
+                .description("Microsserviço de gestão de usuários")
+
+                .termsOfService("http://swagger.io/terms/")
+
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
     }
 
 }
--- src/main/java/br/com/acrtech/planningpoker/usuarioms/controller/UsuarioController.java
+++ src/main/java/br/com/acrtech/planningpoker/usuarioms/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package br.com.acrtech.planningpoker.usuarioms.controller;
 import br.com.acrtech.planningpoker.usuarioms.dto.UsuarioDto;
 import br.com.acrtech.planningpoker.usuarioms.exception.UsuarioNaoEncontradoException;
 import br.com.acrtech.planningpoker.usuarioms.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,20 +28,32 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
-
+    @Operation(
+        summary = "Salva um usuário",
+        description = "Este endpoint tem por objetivo possibilitar a criação de novos usuários dentro do sistema. " +
+            "Os campos nome, e-mail e senha são obrigatórios. A senha não pode ter menos de 6 caracteres."
+    )
     @PostMapping
     public ResponseEntity<UsuarioDto> save(@RequestBody @Valid UsuarioDto usuario){
         log.info("Salvando usuário {}", usuario.getNome());
         return ResponseEntity.ok(usuarioService.save(usuario));
     }
 
+    @Operation(
+        summary = "Lista todos os usuários",
+        description = "Este endpoint tem por objetivo listar todos os usuários cadastrados no sistema"
+    )
     @GetMapping
     public ResponseEntity<List<UsuarioDto>> findAll(){
         return ResponseEntity.ok(usuarioService.findAll());
     }
 
+    @Operation(
+        summary = "Lista um usuário específico baseado em seu UUID",
+        description = "Este endpoint tem por objetivo listar um usuário específico cadastrado no sistema baseado em seu UUID"
+    )
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable UUID id){
+    public ResponseEntity<UsuarioDto> findById(@Parameter(description = "UUID do usuário a ser pesquisado") @PathVariable UUID id){
         try {
             return ResponseEntity.ok(usuarioService.findById(id));
         } catch (UsuarioNaoEncontradoException e) {
@@ -49,8 +63,12 @@ public class UsuarioController {
         }
     }
 
+    @Operation(
+        summary = "Deleta um usuário específico baseado em seu UUID",
+        description = "Este endpoint tem por objetivo deletar um usuário específico cadastrado no sistema baseado em seu UUID"
+    )
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteById(@PathVariable UUID id){
+    public ResponseEntity<HttpStatus> deleteById(@Parameter(description = "UUID do usuário a ser excluido") @PathVariable UUID id){
         usuarioService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
